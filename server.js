ae6b0a9ad5469a81b442353cfbedc252f6b9a9c2
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-02-17 00:26:21 -0600

    changed user.body.accountType to user.accountType

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ server.post('/api/register', async (req, res) => {
   const user = req.body;
   const { username, password, accountType, fname, lname, jobTitle } = user;
 
-  if (user.body.accountType === 'worker') {
+  if (user.accountType === 'worker') {
     if (!(username && password && accountType && fname && lname && jobTitle)) {
       return res.status(400).send('Please fill in all required fields');
     }
